cc9b6b6c07de884cb625c0ab7a75b1bbf46d196a
yuriygb02-dev <yuriygb02@gmail.com>
2026-01-19 14:54:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,44 +1,28 @@
 /* eslint no-control-regex: "off", no-unused-vars: ["error", { "varsIgnorePattern": "stdout*" }] */
 
-// Runtime parameters
-global.prod = (process.env.NODE_ENV === "production") ? true : false;
-
 // Requires
-require('ejs');
-var express = require("express");
-var app = express();
-var compression = require('compression');
-var minify = require('express-minify');
-var path = require('path');
+require("ejs");
+const express = require("express");
+const app = express();
+const compression = require("compression");
+const minify = require("express-minify");
+const path = require("path");
 const helmet = require("helmet");
-const fs = require("fs");
-const router = require('./src/routes.js');
+const router = require("./src/routes.js");
 
-const options = global.prod ? {
-  key: fs.readFileSync('certs/privkey.pem'),
-  cert: fs.readFileSync('certs/fullchain.pem')
-} : {};
-const https = global.prod ? require("https").createServer(options, app)
-                   : require("http").createServer(app);
-
-app.set('view engine', 'ejs');
+// Config
+app.set("view engine", "ejs");
 app.use(compression());
-if (global.prod) { app.use(minify()); }
+app.use(minify());
 app.use(express.json());
 app.use(helmet());
-app.use('/', router);
+app.use("/", router);
 
 global.appRoot = path.resolve(__dirname);
 
-https.listen(8080, function () {
-    console.log("https listening on *:8080\n");
-});
+// 🚀 PORTA CORRETA PARA O RENDER
+const PORT = process.env.PORT || 3000;
 
-if (global.prod) {
-    // Redirect from http port 80 to https
-    var http  = require("http");
-    http.createServer(function (req, res) {
-        res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-        res.end();
-    }).listen(3000);
-}
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
+});
